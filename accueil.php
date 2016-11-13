591fb7d06dcf33120c3bf2284d7aa6182ea58deb
Wiss <wiki59@live.fr>
2016-11-13 02:06:00 +0100

    Connexion page appears first now

--- accueil.php
+++ accueil.php
@@ -11,38 +11,48 @@
     </title>
 </head>
 <body>
-<div id="game">
+<div id="mainDiv">
     <div id="centeredDiv">
         <h1 id="title">
             L'invasion des morbaks
         </h1>
-        <div id="tablePoint">
-            <table id="morbak">
-                <tr>
-                    <td id="11">?</td>
-                    <td id="12">?</td>
-                    <td id="13">?</td>
-                </tr>
-                <tr>
-                    <td id="21">?</td>
-                    <td id="22">?</td>
-                    <td id="23">?</td>
-                </tr>
-                <tr>
-                    <td id="31">?</td>
-                    <td id="32">?</td>
-                    <td id="33">?</td>
-                </tr>
-            </table>
-            <div id="result">
-                Coucou
+
+        <div id="connexion">
+            <input id="pseudo" type="text" placeholder="Insert your name here"/>
+    <!--
+            <div id="checkBox">
+                <input type="radio"/>Se souvenir de moi
             </div>
+     -->
+            <input id="submit" type="submit" value="Jouer"/>
         </div>
-        <br/>
-        <button id="jouer">Lancer les dés</button>
+
+        <div id="game" hidden="hidden">
+            <div id="tablePoint">
+                <table id="morbak">
+                    <tr>
+                        <td id="11">?</td>
+                        <td id="12">?</td>
+                        <td id="13">?</td>
+                    </tr>
+                    <tr>
+                        <td id="21">?</td>
+                        <td id="22">?</td>
+                        <td id="23">?</td>
+                    </tr>
+                    <tr>
+                        <td id="31">?</td>
+                        <td id="32">?</td>
+                        <td id="33">?</td>
+                    </tr>
+                </table>
+                <div id="result">
+                </div>
+            </div>
+            <br/>
+            <button id="jouer">Lancer les dés</button>
+        </div>
     </div>
-</div>
-<div id="score">
 </div>
 <script type="text/javascript" src="script/script.js"></script>
 </body>
